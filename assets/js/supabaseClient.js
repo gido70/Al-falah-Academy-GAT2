ed1863708d678ef74a9190c0ff097dd38fd70e31
gido70 <63196885+gido70@users.noreply.github.com>
2026-05-01 02:03:20 +0400

    Add files via upload

---
+++ assets/js/supabaseClient.js
@@ -0,0 +1,3 @@
+window.ALFA_SUPABASE_URL = 'https://nmbbahzzogspuuvpsxud.supabase.co';
+window.ALFA_SUPABASE_KEY = 'sb_publishable_OHbaA9Rse47v5pw_0Juafg_RbeorWMM';
+window.sb = window.supabase.createClient(window.ALFA_SUPABASE_URL, window.ALFA_SUPABASE_KEY);
